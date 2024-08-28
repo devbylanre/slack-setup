d781f6770d118ae1336ac1476332380a3eacbf13
devbylanre <devbylanre@gmail.com>
2024-08-28 09:15:31 +0100

    chnaged icon

--- src/templates/Active.tsx
+++ src/templates/Active.tsx
@@ -14,16 +14,46 @@ const Active = () => {
         height='1em'
         width='1em'
         xmlns='http://www.w3.org/2000/svg'
-        style={{ width: '20px', height: '20px' }}
       >
         <polygon
-          fill='#FFC107'
-          points='33,22 23.6,22 30,5 19,5 13,26 21.6,26 17,45'
+          fill='#CFD8DC'
+          points='39.4,23 38.6,19 26,21.6 26,8 22,8 22,20.3 8.1,11.3 5.9,14.7 21.1,24.5 9.4,39.8 12.6,42.2 23.9,27.4 32.3,40.1 35.7,37.9 27.3,25.4'
         ></polygon>
+        <circle
+          fill='#3F51B5'
+          cx='24'
+          cy='24'
+          r='7'
+        ></circle>
+        <g fill='#00BCD4'>
+          <circle
+            cx='24'
+            cy='8'
+            r='5'
+          ></circle>
+          <circle
+            cx='39'
+            cy='21'
+            r='5'
+          ></circle>
+          <circle
+            cx='7'
+            cy='13'
+            r='5'
+          ></circle>
+          <circle
+            cx='11'
+            cy='41'
+            r='5'
+          ></circle>
+          <circle
+            cx='34'
+            cy='39'
+            r='5'
+          ></circle>
+        </g>
       </svg>
-      <h6 style={{ flex: '1 1 0%', fontSize: '1.3rem', fontWeight: 600 }}>
-        Active
-      </h6>
+      <h6>Active</h6>
     </Card>
   );
 };
